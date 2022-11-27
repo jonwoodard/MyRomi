c8d3a0b55a983218c27c488f7df0db89f5b0b1a9
jwoodard <jwoodard@lpslancer.net>
2022-11-27 13:30:38 -0500

    updated for periodic updates

--- src/main/java/frc/robot/subsystems/RomiDrivetrain.java
+++ src/main/java/frc/robot/subsystems/RomiDrivetrain.java
@@ -2,86 +2,239 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static frc.robot.Constants.Drive;
 
-public class RomiDrivetrain extends DifferentialDrive
+public class RomiDrivetrain extends SubsystemBase implements RomiSubsystem
 {
   static
   {
     System.out.println("RomiDrivetrain");
   }
 
-  private static final double COUNTS_PER_REV = 1440.0;
-  private static final double WHEEL_DIAMETER_INCH = 70.0 * (1.0 / 25.4); // 70.0 mm * (1.0 in / 25.4 mm)
-  private static final double ROBOT_TRACK_WIDTH_INCH = 141.0 * (1.0 / 25.4);  // 141.0 mm * (1.0 in / 25.4 mm)
+  private enum DriveState
+  {
+    kNONE, kARCADE, kTANK;
+  }
+
+  private enum SpeedFactor
+  {
+    kSLOW(0.75), kFAST(1.0);
+
+    public final double value;
+
+    private SpeedFactor(double value)
+    {
+      this.value = value;
+    }
+  }
 
   // The Romi has the left and right motors set to
   // PWM channels 0 and 1 respectively
-  private static final Talon leftMotor = new Talon(0);
-  private static final Talon rightMotor = new Talon(1);
+  private final Talon leftMotor = new Talon(0);
+  private final Talon rightMotor = new Talon(1);
 
   // The Romi has onboard encoders that are hardcoded
   // to use DIO pins 4/5 and 6/7 for the left and right
-  private static final Encoder leftEncoder = new Encoder(4, 5);
-  private static final Encoder rightEncoder = new Encoder(6, 7);
+  private final Encoder leftEncoder = new Encoder(4, 5);
+  private final Encoder rightEncoder = new Encoder(6, 7);
+  private final Timer encoderResetTimer = new Timer();
 
   // Set up the differential drive controller
-  // private final DifferentialDrive diffDrive = new DifferentialDrive(leftMotor, rightMotor);
+  private final DifferentialDrive diffDrive = new DifferentialDrive(leftMotor, rightMotor);
+ 
+  private DriveState driveState = DriveState.kNONE;
+  private SpeedFactor speedFactor = SpeedFactor.kSLOW;
+
+  // Periodic INPUT
+  private boolean resetEncoders;
+  private boolean isEncoderReset;
+  private double leftEncoderDistance;
+  private double rightEncoderDistance;
+  private int leftEncoderRaw;
+  private int rightEncoderRaw;
+
+  // Periodic OUTPUT
+  private double arcadeDriveSpeed;
+  private double arcadeDriveRotate;
+  private double tankDriveLeftSpeed;
+  private double tankDriveRightSpeed;
+
 
   /** Creates a new RomiDrivetrain. */
   public RomiDrivetrain() 
   {
-    super(leftMotor, rightMotor);
+    // super(leftMotor, rightMotor);
+    diffDrive.setSafetyEnabled(false);
 
     // Use inches as unit for encoder distances
-    leftEncoder.setDistancePerPulse((Math.PI * WHEEL_DIAMETER_INCH) / COUNTS_PER_REV);
-    rightEncoder.setDistancePerPulse((Math.PI * WHEEL_DIAMETER_INCH) / COUNTS_PER_REV);
+    leftEncoder.setDistancePerPulse((Math.PI * Drive.WHEEL_DIAMETER_INCH) / Drive.COUNTS_PER_REV);
+    rightEncoder.setDistancePerPulse((Math.PI * Drive.WHEEL_DIAMETER_INCH) / Drive.COUNTS_PER_REV);
     resetEncoders();
+    encoderResetTimer.reset();
+    encoderResetTimer.start();
 
     // Invert right side since motor is flipped
     rightMotor.setInverted(true);
 
-    SendableRegistry.addLW(this, "RomiDrivetrain", "Motors");
+    // SendableRegistry.addLW(this, "RomiDrivetrain", "Motors");
+    SendableRegistry.addLW(diffDrive, "RomiDrivetrain", "Motors");
     SendableRegistry.addLW(leftEncoder, "RomiDrivetrain", "Encoder Left");
     SendableRegistry.addLW(rightEncoder, "RomiDrivetrain", "Encoder Right");
+
+    diffDrive.setSafetyEnabled(true);
   }
 
-  // public void arcadeDrive(double xaxisSpeed, double zaxisRotate) 
-  // {
-  //   diffDrive.arcadeDrive(xaxisSpeed, zaxisRotate);
-  // }
+ public void toggleSpeedFactor()
+  {
+    switch(speedFactor)
+    {
+      case kSLOW:
+        speedFactor = SpeedFactor.kFAST;
+        break;
+      case kFAST:
+        speedFactor = SpeedFactor.kSLOW;
+        break;
+    }
+  }
+
+  private double truncate(double value, int precision)
+  {
+    double power = Math.pow(10, precision);
+    return Math.floor(value * power) / power;
+  }
+
+  public void arcadeDrive(double speed, double rotate) 
+  {
+    driveState = DriveState.kARCADE;
+    arcadeDriveSpeed = truncate(speed * speedFactor.value, 2);
+    arcadeDriveRotate = truncate(rotate * speedFactor.value, 2);
+
+    tankDriveLeftSpeed = 0.0;
+    tankDriveRightSpeed = 0.0;
+    // diffDrive(speed, rotate);
+  }
+
+  public void tankDrive(double leftSpeed, double rightSpeed)
+  {
+    driveState = DriveState.kTANK;
+    tankDriveLeftSpeed = truncate(leftSpeed * speedFactor.value, 2);
+    tankDriveRightSpeed = truncate(rightSpeed * speedFactor.value, 2);
+
+    arcadeDriveSpeed = 0.0;
+    arcadeDriveRotate = 0.0;
+    // diffDrive.tankDrive(leftSpeed, rightSpeed);
+  }
+
+  public void stopMotors()
+  {
+    driveState = DriveState.kNONE;
+    arcadeDriveSpeed = 0.0;
+    arcadeDriveRotate = 0.0;
+    tankDriveLeftSpeed = 0.0;
+    tankDriveRightSpeed = 0.0;
+    // diffDrive.stopMotor();
+  }
 
   public void resetEncoders() 
   {
-    leftEncoder.reset();
-    rightEncoder.reset();
+    driveState = DriveState.kNONE;
+    resetEncoders = true;
+    isEncoderReset = false;
+    encoderResetTimer.reset();
+    encoderResetTimer.start();
+    // leftEncoder.reset();
+    // rightEncoder.reset();
   }
 
   public double getLeftSpinDegree()
   {
-    return leftEncoder.getDistance() / (Math.PI * ROBOT_TRACK_WIDTH_INCH) * 360.0;
+    return leftEncoderDistance / (Math.PI * Drive.ROBOT_TRACK_WIDTH_INCH) * 360.0;
+    // return leftEncoder.getDistance() / (Math.PI * ROBOT_TRACK_WIDTH_INCH) * 360.0;
   }
 
   public double getRightSpinDegree()
   {
-    return rightEncoder.getDistance() / (Math.PI * ROBOT_TRACK_WIDTH_INCH) * 360.0;
+    return rightEncoderDistance / (Math.PI * Drive.ROBOT_TRACK_WIDTH_INCH) * 360.0;
+    // return rightEncoder.getDistance() / (Math.PI * ROBOT_TRACK_WIDTH_INCH) * 360.0;
   }
   
   public double getLeftDistanceInch() 
   {
-    return leftEncoder.getDistance();
+    return leftEncoderDistance;
+    // return leftEncoder.getDistance();
   }
 
   public double getRightDistanceInch() 
   {
-    return rightEncoder.getDistance();
+    return rightEncoderDistance;
+    // return rightEncoder.getDistance();
   }
 
-  // public boolean isDriveInchDone(double distance)
-  // {
-  //   return getLeftDistanceInch() < distance && getRightDistanceInch() < distance;
-  // }
+  public double getAverageDistanceInch()
+  {
+    return (leftEncoderDistance + rightEncoderDistance) / 2.0;
+  }
+
+  public boolean isEncoderReset()
+  {
+    // System.out.println("LE = " + leftEncoderRaw + " RE = " + rightEncoderRaw);
+
+    return isEncoderReset;
+  }
+
+  @Override
+  public synchronized void readPeriodicInputs()
+  {
+    leftEncoderDistance = leftEncoder.getDistance();
+    rightEncoderDistance = rightEncoder.getDistance();
+    leftEncoderRaw = leftEncoder.get();
+    rightEncoderRaw = rightEncoder.get();
+  }
+
+  @Override
+  public synchronized void writePeriodicOutputs()
+  {
+    if(resetEncoders)
+    {
+      diffDrive.stopMotor();
+      leftEncoder.reset();
+      rightEncoder.reset();
+      resetEncoders = false;
+    }
+    else if(!isEncoderReset)
+    {
+      if(leftEncoderRaw == 0 && rightEncoderRaw == 0)
+        isEncoderReset = true;
+      else if(encoderResetTimer.hasElapsed(0.1))
+      {
+        leftEncoder.reset();
+        rightEncoder.reset();
+        encoderResetTimer.reset();
+        encoderResetTimer.start();
+      }
+    }
+    else
+    {
+      switch(driveState)
+      {
+        case kARCADE:
+          diffDrive.arcadeDrive(arcadeDriveSpeed, arcadeDriveRotate);
+          break;
+        case kTANK:
+          diffDrive.tankDrive(tankDriveLeftSpeed, tankDriveRightSpeed);
+          break;
+        case kNONE:
+          diffDrive.stopMotor();
+          break;
+      }
+    }
+
+    diffDrive.feedWatchdog();
+  }
 
   public String toString()
   {
