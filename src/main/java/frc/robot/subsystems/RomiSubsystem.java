da63d4a94158456227429fbca541e5b409b9d8e6
jwoodard <jwoodard@lpslancer.net>
2022-11-27 13:29:16 -0500

    interface used for periodic updates

---
+++ src/main/java/frc/robot/subsystems/RomiSubsystem.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems;
+
+// https://github.com/Team254/FRC-2020-Public
+
+public interface RomiSubsystem 
+{
+  public abstract void readPeriodicInputs();
+  public abstract void writePeriodicOutputs();
+
+  public default void enablePeriodicUpdates(boolean isEnabled)
+  {}
+
+  public default boolean isPeriodicUpdateEnabled()
+  {
+    return true;
+  }
+}
