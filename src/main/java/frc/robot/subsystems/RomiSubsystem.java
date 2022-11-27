bfad06f8497e6b51830146d301ca750b8d21a487
jwoodard <jwoodard@lpslancer.net>
2022-11-27 14:27:55 -0500

    comments

--- src/main/java/frc/robot/subsystems/RomiSubsystem.java
+++ src/main/java/frc/robot/subsystems/RomiSubsystem.java
@@ -2,6 +2,9 @@ package frc.robot.subsystems;
 
 // https://github.com/Team254/FRC-2020-Public
 
+/**
+ * Interface to periodically update inputs/outputs for the romi subsystem
+ */
 public interface RomiSubsystem 
 {
   public abstract void readPeriodicInputs();
