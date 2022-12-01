abf97b1669afcd86c625ef3e48aa4e56538fd8e4
jwoodard <jwoodard@lpslancer.net>
2022-12-01 07:23:15 -0500

    new wrapper classes to use our interface

---
+++ src/main/java/frc/robot/subsystems/RomiAccelerometer.java
@@ -0,0 +1,71 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.util.sendable.SendableRegistry;
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
+
+public class RomiAccelerometer implements RomiSubsystem
+{
+  private final BuiltInAccelerometer accelerometer = new BuiltInAccelerometer();
+
+  private double x;
+  private double y;
+  private double z;
+
+  public RomiAccelerometer()
+  {
+    SendableRegistry.addLW(accelerometer, "RomiAccelerometer", "Accelerometer");
+  }
+
+  /**
+   * Method to get the acceleration in the x direction
+   * @return the x acceleration
+   */
+  public double getX()
+  {
+    return x;
+  }
+
+  /**
+   * Method to get the acceleration in the y direction
+   * @return the y acceleration
+   */
+  public double getY()
+  {
+    return y;
+  }
+
+  /**
+   * Method to get the acceleration in the z direction
+   * @return the z acceleration
+   */
+  public double getZ()
+  {
+    return z;
+  }
+
+  /**
+   * Method that is called in robotPeriodic() to read inputs
+   */
+  @Override
+  public synchronized void readPeriodicInputs()
+  {
+    x = accelerometer.getX();
+    y = accelerometer.getY();
+    z = accelerometer.getZ();
+  }
+  
+  /**
+   * Method that is called in robotPeriodic() to write outputs
+   */
+  @Override
+  public synchronized void writePeriodicOutputs()
+  {}
+
+  @Override
+  public String toString()
+  {
+    String str = String.format("Accel: x = %5.1f | y = %5.1f | z = %5.1f", x, y, z);
+    return str;
+  }
+}
+
---
+++ src/main/java/frc/robot/subsystems/RomiGyro4237.java
@@ -0,0 +1,95 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.romi.RomiGyro;
+
+public class RomiGyro4237 implements RomiSubsystem
+{
+  private final RomiGyro gyro = new RomiGyro();
+  private final Timer gyroResetTimer = new Timer();
+  
+  // Periodic INPUT
+  private double angle;
+
+  // Periodic OUTPUT
+  private boolean resetGyro;
+  private boolean isGyroReset;
+
+
+  public RomiGyro4237()
+  {
+    reset();
+  }
+
+  /**
+   * Method to reset the gyro
+   * NOTE: resetting the gyro takes time, it is not instantaneous
+   * NOTE: use the method isGyroReset() to check if the gyro has reset
+   */
+  public void reset()
+  {
+    resetGyro = true;
+    isGyroReset = false;
+    gyroResetTimer.reset();
+    gyroResetTimer.start();
+  }
+
+  /**
+   * Method to check if the gyro have been reset
+   * @return true when the gyro is reset
+   */
+  public boolean isGyroReset()
+  {
+    return isGyroReset;
+  }
+
+  /**
+   * Method to get the gyro angle
+   * @return the angle of the gyro
+   */
+  public double getAngle()
+  {
+    return angle;
+  }
+
+  /**
+   * Method that is called in robotPeriodic() to read inputs
+   */
+  @Override
+  public synchronized void readPeriodicInputs()
+  {
+    angle = gyro.getAngle();
+  }
+
+  /**
+   * Method that is called in robotPeriodic() to write outputs
+   */
+  @Override
+  public synchronized void writePeriodicOutputs()
+  {
+    if(resetGyro)
+    {
+      gyro.reset();
+      resetGyro = false;
+    }
+    else if(!isGyroReset)
+    {
+      if(Math.abs(angle) <= 0.5)
+        isGyroReset = true;
+      else if(gyroResetTimer.hasElapsed(0.1))
+      {
+        gyro.reset();
+        gyroResetTimer.reset();
+        gyroResetTimer.start();
+      }
+    }
+  }
+
+  @Override
+  public String toString()
+  {
+    String str = String.format("Gyro: angle = %5.1f", angle);
+    return str;
+  }
+}
+
