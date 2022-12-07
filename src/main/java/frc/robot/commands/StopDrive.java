beed2b56e14cc5cb0f4f065a1ad9d81a4e44fdb3
jwoodard <jwoodard@lpslancer.net>
2022-12-06 20:32:53 -0500

    new command to stop driving

---
+++ src/main/java/frc/robot/commands/StopDrive.java
@@ -0,0 +1,38 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.RomiDrivetrain;
+
+public class StopDrive extends CommandBase
+{
+  private final RomiDrivetrain drivetrain;
+
+  public StopDrive(RomiDrivetrain drivetrain)
+  {
+    this.drivetrain = drivetrain;
+
+    if(this.drivetrain != null)
+      addRequirements(drivetrain);
+  }
+
+  @Override
+  public void initialize()
+  {
+    if(drivetrain != null)
+      drivetrain.stopMotors();
+  }
+
+  @Override
+  public void execute()
+  {}
+
+  @Override
+  public void end(boolean interrupted)
+  {}
+
+  @Override
+  public boolean isFinished()
+  {
+    return true;
+  }
+}
